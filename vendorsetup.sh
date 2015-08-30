9fddb2b9af6a3c287ffbfb98407887bd1cea2208
Benowit <benowit@gmail.com>
2015-08-30 21:06:59 +0200

    My vendor step1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-12.1 | awk '{printf "cm_%s-%s\n", $1, $2}')
+for combo in $(cat vendor/cm/cb-build-targets)
 do
     add_lunch_combo $combo
 done
